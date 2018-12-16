05cfe5830c563991e6c67ea0dd97ad18049ec845
Harshit Gupta <harshitsaamu@gmail.com>
2018-12-17 01:44:59 +0530

    fixing apps state after clicking x button

--- Brisk/AppDelegate.swift
+++ Brisk/AppDelegate.swift
@@ -10,13 +10,23 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag{
+            for window in sender.windows{
+                if let w = window as? NSWindow{
+                    w.makeKeyAndOrderFront(self)
+                }
+            }
+        }
+        return true
     }
 }
 
